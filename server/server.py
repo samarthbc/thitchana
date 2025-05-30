876094747a4c816162934353ca512dbf7f4922a7
unknownalien1 <samarthbellam@gmail.com>
2025-05-30 20:22:37 +0530

    Commit 6

--- server/server.py
+++ server/server.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from pymongo import MongoClient
 from pymongo.errors import PyMongoError
 
 app = FastAPI()
+
+# Allow all CORS origins (not secure for production!)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all HTTP methods
+    allow_headers=["*"],  # Allow all headers
+)
 
 # MongoDB client setup
 client = MongoClient("mongodb+srv://user1:User1@cluster0.o4gvz5k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
